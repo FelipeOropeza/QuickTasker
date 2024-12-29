14f52b784ec69dd3b78bebfb7347f82635009184
FelipeOropeza <felipe2006.co@gmail.com>
2024-12-29 09:37:09 -0300

    feat: pegando os valores do input do usuario e enviar os dados por meio de uma requisição POST pra uma API

--- src/components/CadastroForm.jsx
+++ src/components/CadastroForm.jsx
@@ -1,29 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function CadastroForm() {
+function CadastroForm({ onCadastro }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userData = { name, email, password };
+    onCadastro(userData);
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-3 bg-white rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold text-center">Cadastro</h1>
-        <form className="space-y-6">
+        <h1 className="text-2xl font-bold text-center">Cadastrar</h1>
+        <form className="space-y-6" onSubmit={handleSubmit}>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Nome</span>
             </label>
-            <input type="text" placeholder="Nome" className="input input-bordered w-full" />
+            <input
+              type="text"
+              placeholder="Nome"
+              className="input input-bordered w-full"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
-            <input type="email" placeholder="Email" className="input input-bordered w-full" />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input input-bordered w-full"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
-            <input type="password" placeholder="Password" className="input input-bordered w-full" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="input input-bordered w-full"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <button type="submit" className="btn btn-primary w-full">Cadastrar</button>
         </form>
--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
-import CadastroForm from '../components/CadastroForm';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import CadastroForm from "../components/CadastroForm";
+
+import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 function Cadastro() {
-  return <CadastroForm />;
+  const [message, setMessage] = useState("");
+  console.log(apiUrl);
+  const navigate = useNavigate();
+
+  const handleCadastro = async (userData) => {
+    try {
+      const response = await axios.post(
+        `${apiUrl}/users`,
+        userData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        setMessage("Cadastro realizado com sucesso");
+        setTimeout(() => {
+          navigate("/");
+        }, 2000);
+      } else {
+        setMessage("Erro ao realizar o cadastro");
+      }
+    } catch (error) {
+      setMessage("Erro ao realizar o cadastro");
+    }
+  };
+
+  return (
+    <div>
+      {message && <p className="text-center text-red-500">{message}</p>}
+      <CadastroForm onCadastro={handleCadastro} />
+    </div>
+  );
 }
 
 export default Cadastro;
