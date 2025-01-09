963572fc3619744b9670066d9f3dc6b9d05a1d73
FelipeOropeza <felipe2006.co@gmail.com>
2025-01-09 20:59:56 -0300

    feat: melhoria nas resostas de erro e sucesso

--- src/components/CadastroForm.jsx
+++ src/components/CadastroForm.jsx
@@ -1,64 +1,80 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function CadastroForm({ onCadastro }) {
+function CadastroForm({ onCadastro, message }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setIsSubmitting(true);
     const userData = { name, email, password };
     onCadastro(userData);
+    setIsSubmitting(false);
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-3 bg-white rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold text-center">Cadastrar</h1>
-        <form className="space-y-6" onSubmit={handleSubmit}>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Nome</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Nome"
-              className="input input-bordered w-full"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="Email"
-              className="input input-bordered w-full"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="Password"
-              className="input input-bordered w-full"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary w-full">Cadastrar</button>
-        </form>
-        <p className="text-center">
-          Já tem uma conta? <Link to="/" className="text-blue-500">Faça login</Link>
+    <div className="w-full max-w-md p-8 space-y-3 bg-white rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold text-center">Cadastrar</h1>
+      {message && (
+        <p
+          className={`text-center ${
+            message.includes("sucesso") ? "text-green-500" : "text-red-500"
+          }`}
+        >
+          {message}
         </p>
-      </div>
+      )}
+      <form className="space-y-6" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Nome</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Nome"
+            className="input input-bordered w-full"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="Email"
+            className="input input-bordered w-full"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="Password"
+            className="input input-bordered w-full"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary w-full"
+          disabled={isSubmitting}
+        >
+          Cadastrar
+        </button>
+      </form>
+      <p className="text-center">
+        Já tem uma conta? <Link to="/" className="text-blue-500">Faça login</Link>
+      </p>
     </div>
   );
 }
--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CadastroForm from "../components/CadastroForm";
-
 import axios from "axios";
+
 const apiUrl = import.meta.env.VITE_API_URL;
 
 function Cadastro() {
   const [message, setMessage] = useState("");
-  console.log(apiUrl);
   const navigate = useNavigate();
 
   const handleCadastro = async (userData) => {
@@ -22,23 +21,26 @@ function Cadastro() {
         }
       );
 
-      if (response.status === 200) {
+      if (response.status === 201) {
         setMessage("Cadastro realizado com sucesso");
         setTimeout(() => {
+          setMessage("");
           navigate("/");
         }, 2000);
+      }
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.message) {
+        setMessage(error.response.data.message);
       } else {
         setMessage("Erro ao realizar o cadastro");
       }
-    } catch (error) {
-      setMessage("Erro ao realizar o cadastro");
+      setTimeout(() => setMessage(""), 3000);
     }
   };
 
   return (
-    <div>
-      {message && <p className="text-center text-red-500">{message}</p>}
-      <CadastroForm onCadastro={handleCadastro} />
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <CadastroForm onCadastro={handleCadastro} message={message} />
     </div>
   );
 }
