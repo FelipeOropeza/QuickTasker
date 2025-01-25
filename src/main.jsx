e0f071b5389d883b054d96ffa447c688fc8c039e
FelipeOropeza <felipe2006.co@gmail.com>
2025-01-25 01:26:06 -0300

    feat: configuração da query

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import "./index.css";
 
 import AppRoutes from "./routes/routes.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <AuthProvider>
-      <AppRoutes />
-    </AuthProvider>
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </StrictMode>
+  </QueryClientProvider>
 );
