17dd987caae230003594297dc3f091e7555b2943
FelipeOropeza <felipe2006.co@gmail.com>
2025-01-25 01:26:54 -0300

    refactor: tirando a logiça de adicionar uma taks na Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
-import SideBar from '../components/SideBar';
-import AddTask from '../components/AddTask';
-import TaskList from '../components/TaskList';
+import React, { useState, useContext } from "react";
+import SideBar from "../components/SideBar";
+import AddTask from "../components/AddTask";
+import { AuthContext } from "../context/AuthContext";
+import TaskList from "../components/TaskList";
 
 const Home = () => {
+  const { token, userId } = useContext(AuthContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleAddTask = (task) => {
-    setIsModalOpen(false);
-  };
 
   return (
     <div className="flex h-screen">
       <SideBar />
 
       <div className="flex-1 p-6 overflow-auto">
-        <h1 className="text-3xl font-bold">Home Page</h1>
 
         <button
           className="w-12 h-12 bg-blue-500 text-white rounded-full flex items-center justify-center text-2xl fixed bottom-6 right-6"
@@ -24,7 +21,13 @@ const Home = () => {
           +
         </button>
 
-        {isModalOpen && <AddTask onAddTask={handleAddTask} setIsModalOpen={setIsModalOpen} />}
+        {isModalOpen && (
+          <AddTask
+            setIsModalOpen={setIsModalOpen}
+            token={token}
+            userId={userId}
+          />
+        )}
 
         <TaskList />
       </div>
